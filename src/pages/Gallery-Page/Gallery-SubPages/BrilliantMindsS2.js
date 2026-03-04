4c4120963d144d47299a2a3706b55a78182c7216
graybruer <gbruer@gmail.com>
2026-03-04 17:46:58 -0500

    fixing gh-pages hosting part 1

---
+++ src/pages/Gallery-Page/Gallery-SubPages/BrilliantMindsS2.js
@@ -0,0 +1,23 @@
+
+import placeholder from '../../../assets/placeholder.png';
+import placeholder_video2 from '../../../assets/Project Pages/flower.webm'
+
+import GallerySubpage from '../../../components/gallery-subpage/GallerySubpage';
+
+
+const BrilliantMinds = () => {
+
+    return (
+
+        <GallerySubpage Video={placeholder_video2}
+                        Title="Brilliant Minds - Season 2"
+                        Date="2025"
+                        Role="1st Assistant Art Director"
+                        Details="This is what I worked on for this job blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah enter content here"
+                        Image1={placeholder}
+        />
+
+    );
+};
+
+export default BrilliantMinds;
---
+++ src/pages/Gallery-Page/Gallery-SubPages/SterlingPointS1.js
@@ -0,0 +1,23 @@
+
+import placeholder from '../../../assets/placeholder.png';
+import placeholder_video2 from '../../../assets/Project Pages/flower.webm'
+
+import GallerySubpage from '../../../components/gallery-subpage/GallerySubpage';
+
+
+const SterlingPoint = () => {
+
+    return (
+
+        <GallerySubpage Video={placeholder_video2}
+                        Title="Sterling Point - Season 1"
+                        Date="2025"
+                        Role="1st Assistant Art Director"
+                        Details="This is what I worked on for this job blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah enter content here"
+                        Image1={placeholder}
+        />
+
+    );
+};
+
+export default SterlingPoint;
