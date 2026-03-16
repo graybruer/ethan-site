766df5d15fba57a1e64323d90f2fd8562562ed1e
graybruer <gbruer@gmail.com>
2026-03-16 16:30:41 -0400

    finalized about page

--- src/pages/About-Page/About.js
+++ src/pages/About-Page/About.js
@@ -1,6 +1,6 @@
 import './About.css'
 import './About-Breakpoints.css'
-import Ethan from '../../assets/Ethan-Placeholder.jpg';
+import Ethan from '../../assets/IMG_7270_smaller.jpeg';
 import { motion } from 'framer-motion';
 
 const About = () => {
@@ -17,14 +17,13 @@ const About = () => {
                 <img src={Ethan} alt="Ethaniel Snow" className='Portrait'></img>
                 <article>
                 <p>
-                    Ethan Snow is an Assistant Art Director who has worked on numerous projects.
-                    Outside of film, he works on the odd residential/commercial interior design & contracting project.
+                    <strong>Ethaniel Snow</strong> is a Toronto-based <strong>Set Designer</strong> working across film, television, and commercial production. 
+                    A graduate of Sheridan College’s Honours Bachelor of Interior Design program, he brings a strong foundation in spatial planning, drafting, and construction knowledge to the art department. 
+                    Additional studies in the fields of fine art, creativity & problem solving, as well as group dynamics blend together to create a diverse skill set.
                 </p>
                 <p>
-                    He currently lives in [insert city here] and is available to work [in area].
-                    With experience a people and projects, strong technical drafting and 3D modelling skills, 
-                    and a strong understanding of construction and building practices, 
-                    Ethan has a generalist skill set and works well as part of a team or individually.
+                    With experience supporting productions from prep through shoot, Snow works closely with production designers and art directors to develop detailed set drawings, generate script breakdowns, coordinate technical documentation, oversee location measures, and assist with build logistics. 
+                    His approach combines strong research, material awareness, and practical construction knowledge to help translate design concepts into functional, camera-ready environments.
                 </p>
                 </article>
             </article>
