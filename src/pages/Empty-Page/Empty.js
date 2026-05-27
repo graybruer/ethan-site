f371e3cdadaddddc50a0509a180f1b9b18575aaa
graybruer <gbruer@gmail.com>
2026-05-27 17:47:00 -0400

    edit to email on empty page error message

--- src/pages/Empty-Page/Empty.js
+++ src/pages/Empty-Page/Empty.js
@@ -3,9 +3,11 @@ import './Empty.css'
 const Empty = (empty) => {
     return (
         <div id="empty">
+            <p>
             This should not be visible. 
             If you are reading this the website is at least mildly broken. 
-            Please let me know at: esnow272@gmail.com.
+            Please let me know at: ethaniel.snow@gmail.com.
+            </p>
         </div>
     );
 };
